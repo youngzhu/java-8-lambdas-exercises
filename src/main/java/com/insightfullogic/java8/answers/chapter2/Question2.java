7b34aa210cbab4acaa7490cadb8759578d4639a5
young-win <young.zhu.2012@gmail.com>
2022-09-15 10:40:22 +0800

    月份表达特殊，所以设定为英语环境

--- src/main/java/com/insightfullogic/java8/answers/chapter2/Question2.java
+++ src/main/java/com/insightfullogic/java8/answers/chapter2/Question2.java
@@ -2,11 +2,15 @@ package com.insightfullogic.java8.answers.chapter2;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import static java.lang.ThreadLocal.withInitial;
 
 public class Question2 {
 
-     public final static ThreadLocal<DateFormat> formatter = withInitial(() -> new SimpleDateFormat("dd-MMM-yyyy"));
+     public final static ThreadLocal<DateFormat> formatter = withInitial(() -> {
+          // 月份表达特殊，所以设定为英语环境
+          return new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+     });
 
 }
