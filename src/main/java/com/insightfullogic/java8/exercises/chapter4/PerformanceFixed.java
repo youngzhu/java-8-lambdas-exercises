bd36c7075db5f3fe35fb5206478bf12df817e9f7
young-win <young.zhu.2012@gmail.com>
2022-09-16 15:22:51 +0800

    ch04

--- src/main/java/com/insightfullogic/java8/exercises/chapter4/PerformanceFixed.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter4/PerformanceFixed.java
@@ -15,7 +15,8 @@ public interface PerformanceFixed {
     public Stream<Artist> getMusicians();
 
     public default Stream<Artist> getAllMusicians() {
-        return Exercises.replaceThisWithSolution();
+        return getMusicians()
+                .flatMap(artist -> concat(Stream.of(artist), artist.getMembers()));
     }
 
 }
